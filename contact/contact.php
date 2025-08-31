1c29193225b5f820857df984e8c2cf53574ec65e
fuhktong <dustint505@gmail.com>
2025-08-31 09:56:24 +0700

    Add GitHub Actions deployment

--- contact/contact.php
+++ contact/contact.php
@@ -26,13 +26,13 @@
 
 <div class="map-container">
     <div class="map-link">
-        <a href="https://maps.app.goo.gl/PXH87qQesxWAWqbKA" target="_blank" rel="noopener noreferrer">
+        <a href="https://maps.app.goo.gl/f1cUKDwtychsicR1A" target="_blank" rel="noopener noreferrer">
             Click here to view Laundry 24 Orlando in Google Maps
         </a>
     </div>
     <div class="static-map">
         <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.8!2d-81.30726034319346!3d28.480998849569467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDI4JzUxLjYiTiA4McKwMTgnMjYuMSJX!5e0!3m2!1sen!2sus!4v1234567890"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1063.137465023263!2d-81.32786334775733!3d28.48019804218407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e763827b2e5d73%3A0x8bab31ab87c160b4!2sLaundry%2024%20Orlando!5e1!3m2!1sen!2skh!4v1756608925572!5m2!1sen!2skh"
             width="90%" 
             height="600" 
             style="border:0; margin: 0 auto; display: block;" 
