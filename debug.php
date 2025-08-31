2704c6f059d7a360ae39094bb7a2071bce31ac16
fuhktong <dustint505@gmail.com>
2025-08-31 07:12:22 +0700

    Add GitHub Actions deployment

---
+++ debug.php
@@ -0,0 +1,24 @@
+<?php
+// Simple debug script to test PHP and dependencies
+echo "PHP Version: " . phpversion() . "<br>";
+echo "Current Directory: " . getcwd() . "<br>";
+echo "Vendor autoload exists: " . (file_exists('vendor/autoload.php') ? 'Yes' : 'No') . "<br>";
+echo "Email helper exists: " . (file_exists('email_helper.php') ? 'Yes' : 'No') . "<br>";
+echo ".env file exists: " . (file_exists('.env') ? 'Yes' : 'No') . "<br>";
+echo "../.env file exists: " . (file_exists('../.env') ? 'Yes' : 'No') . "<br>";
+
+if (file_exists('vendor/autoload.php')) {
+    try {
+        require_once 'vendor/autoload.php';
+        echo "PHPMailer autoload: Success<br>";
+        
+        use PHPMailer\PHPMailer\PHPMailer;
+        $mail = new PHPMailer();
+        echo "PHPMailer instance: Success<br>";
+    } catch (Exception $e) {
+        echo "PHPMailer error: " . $e->getMessage() . "<br>";
+    }
+} else {
+    echo "Vendor autoload not found<br>";
+}
+?>
