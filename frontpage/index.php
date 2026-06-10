0697c50dae912f9e38a30969a750d5e143731c13
fuhktong <dustint505@gmail.com>
2026-06-10 07:44:22 +0700

    Add GitHub Actions deployment

---
+++ frontpage/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laundry 24 Orlando</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="frontpage.css">
+    <link rel="stylesheet" href="../footer/footer.css">
+</head>
+<body class="frontpage-body">
+    <main class="frontpage-main">
+        <div class="frontpage-logo-container">
+            <img src="laundry24logo4.png" alt="Laundry 24 Orlando" />
+        </div>
+
+        <div class="frontpage-contact-info">
+            <h2>Address</h2>
+            <p>
+                <a href="https://maps.app.goo.gl/PXH87qQesxWAWqbKA" target="_blank" rel="noreferrer">
+                    4528 Hoffner Ave<br>Orlando, FL 32812
+                </a>
+            </p>
+
+            <h2>Phone</h2>
+            <p>
+                <a href="tel:+14072379531">(689) 306-3040</a>
+            </p>
+        </div>
+    </main>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-title">
+                <a href="../index.php">
+                    <h2>Laundry 24 Orlando</h2>
+                </a>
+            </div>
+            <div>
+                <ul class="footer-links">
+                    <li><a class="link2" href="../index.php?page=about">ABOUT</a></li>
+                    <li><a class="link2" href="../index.php?page=services">SERVICES</a></li>
+                    <li><a class="link2" href="../index.php?page=laundroworks">LAUNDROWORKS</a></li>
+                    <li><a class="link2" href="../index.php?page=contact">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="logo-footer">
+            <a href="../index.php">
+                <img src="../images/laundry24logo4.png" alt="Home" />
+            </a>
+        </div>
+    </footer>
+</body>
+</html>
